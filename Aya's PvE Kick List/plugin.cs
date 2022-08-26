a92e39d8275749564d91f643809027e8d55ff5d1
luizbrttgms <98783376+luizbrttgms@users.noreply.github.com>
2022-08-25 22:03:35 -0300

    Update plugin.cs

--- Aya's PvE Kick List/plugin.cs
+++ Aya's PvE Kick List/plugin.cs
@@ -144,7 +144,6 @@ namespace AimsharpWow.Modules
             {
                 Interrupts.Add("Rebuke");
                 Interrupts.Add("Avenger's Shield");
-		Interrupts.Add("Hammer of Justice");
 				if (UseCC)
                 {
                     CCInterrupts.Add("Hammer of Justice");
